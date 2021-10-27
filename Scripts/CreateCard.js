14cb05ced4125fed9d2fd4e1284574e2994a1911
Carruor <fotoes10@gmail.com>
2021-10-27 13:31:05 +0200

    JS changes

--- Scripts/CreateCard.js
+++ Scripts/CreateCard.js
@@ -1,5 +1,11 @@
 function createCard()  {
     let boxZombieCards = document.getElementById("zombieList")
-    boxZombieCards +=
+    boxZombieCards.innerHTML+= `<li>
+                                    <img class="imgCards" src="./assets/partida/zoombie/zombi.png" alt="zombie">
+                                    <p class="nameZombie">Name</p>
+                                </li>`
 
 }
+
+
+ 
